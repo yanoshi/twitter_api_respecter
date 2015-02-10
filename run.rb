a18f674763520110413849cde5d03a20a7f5ca3c
yanoshi <googreimu@gmail.com>
2015-02-10 16:12:39 +0900

    exampleを収集することに成功

--- run.rb
+++ run.rb
@@ -3,40 +3,67 @@ require 'open-uri'
 # Nokogiriライブラリの読み込み
 require 'nokogiri'
 
-# TwitterAPIのドキュメントURL
-apidoc_url = 'https://dev.twitter.com/rest/public'
+require 'fileutils'
 
-api_list = []
 
-charset = nil
-html = open(apidoc_url) do |f|
-  charset = f.charset # 文字種別を取得
-  f.read # htmlを読み込んで変数htmlに渡す
+def make_mock(list)
+	p list 
 end
 
-# htmlをパース(解析)してオブジェクトを生成
-doc = Nokogiri::HTML.parse(html, nil, charset)
+def get_example(url)
+	charset = nil
+	html = open(url) do |f|
+		charset = f.charset # 文字種別を取得
+		f.read # htmlを読み込んで変数htmlに渡す
+	end	
+
+	# htmlをパース(解析)してオブジェクトを生成
+	doc = Nokogiri::HTML.parse(html, nil, charset)
 
 
+	examples = doc.xpath('//pre[@class="brush: jscript"]')
+	#syntaxhighlighter nogutter  jscript  
 
-sidemenu = doc.xpath('//div[@class="SystemBlock"]')[0].xpath('//ul[@class="menu"]')
-
-
-
-sidemenu.xpath('//a').each do |apiname|
-
-	if apiname.inner_text.index("GET") != nil
-		tempurl=apiname["href"]
-		api_list.push({ 
-			:url =>  tempurl, 
-			:example => get_example(tempurl) })
-		#p apiname.inner_text
-		#p apiname["href"]
+	#puts examples[0].inner_text
+	if !!examples[0]
+		return examples[0].inner_text
+	else
+		return ""
 	end
 end
 
-p api_list
+def main
+	# TwitterAPIのドキュメントURL
+	apidoc_url = 'https://dev.twitter.com/rest/public'
 
-def get_example(url)
-	return ""
+	api_list = []
+
+	charset = nil
+	html = open(apidoc_url) do |f|
+	  charset = f.charset # 文字種別を取得
+	  f.read # htmlを読み込んで変数htmlに渡す
+	end
+
+	# htmlをパース(解析)してオブジェクトを生成
+	doc = Nokogiri::HTML.parse(html, nil, charset)
+
+	sidemenu = doc.xpath('//div[@class="SystemBlock"]')[0].xpath('//ul[@class="menu"]')
+
+	sidemenu.xpath('//a').each do |apiname|
+
+		if apiname.inner_text.index("GET") != nil
+			tempurl=apiname["href"]
+			api_list.push({ 
+				:url =>  "https://dev.twitter.com" + tempurl,
+				:directory => "api" + tempurl,
+				:example => get_example("https://dev.twitter.com" + tempurl) })
+			#p apiname.inner_text
+			#p apiname["href"]
+		end
+	end
+
+	make_mock(api_list)
 end
+
+
+main
